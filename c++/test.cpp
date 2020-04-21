14e83ab37794853973773a69cf9eec1cac430a24
Gilberto Raitz Junior <gilbertoraitzjr@gmail.com>
2020-04-21 01:54:03 -0300

    python code

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
-using namespace std;
+#include "stdio.h"
 
-int main(){
-    cout<<"hello world";
-    return o;
 
+int main()
+{
+    printf("isso memu");
+
+    system("pause"); // esse comando é para segurar a janela do cmd aberta após imprimir o texto acima
+
+    return 0;
 }
